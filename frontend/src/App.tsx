7e7d6f7da4dff39abc4d90486de8282de88c9b1a
Christopher Maldonado <149415670+chrismald0228@users.noreply.github.com>
2026-05-30 01:31:25 -0500

    implemented shadcn in App.tsx and added user type

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react"
 import { userApi } from "./api/user"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import type { User } from "@/types/user"
 
 function App() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<User[]>([]);
 
   useEffect(() => {
     userApi.getAll().then((res) => setData(res.data));
@@ -10,8 +12,15 @@ function App() {
 
   return (
     <>
-      <h1>Hi</h1>
-      <div>{JSON.stringify(data)}</div>
+      <div className="p-8">
+        {data.map((user) => (
+          <Card key={user.username} className="m-2">
+            <CardTitle className="p-2 font-bold underline decoration-2 decoration-sky-600">{user.username}</CardTitle>
+            <CardHeader>{user.name}</CardHeader>
+            <CardContent>{user.phoneNum}</CardContent>
+          </Card>
+        ))}
+      </div>
     </>
   )
 }
