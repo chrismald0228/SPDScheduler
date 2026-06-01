bef07735a34b20c4336889afdc439f25d110d5b6
Christopher Maldonado <149415670+chrismald0228@users.noreply.github.com>
2026-05-31 23:23:00 -0500

    implemented the custom errors to the main elysia app to resgister them as possible errors.

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,12 +1,14 @@
 import { Elysia } from 'elysia'
 import { user } from './modules/user/user'
 import { reservation } from './modules/reservation/reservation'
+import { errors} from './lib/errors'
 
 export const app = new Elysia({prefix: "/api"})
+    .error(errors)
     .use(user)
     .use(reservation)
 
-
+//does not listen if running test, since bun runs them internally
 if(Bun.env.NODE_ENV !== "test") {
     app.listen(3000)
     console.log(`Server running at http://localhost:${app.server?.port}/api`)
