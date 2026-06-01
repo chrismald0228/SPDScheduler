334e6be999544e1e3171eecd30e50ccf80ecd7d5
Christopher Maldonado <149415670+chrismald0228@users.noreply.github.com>
2026-05-31 23:23:37 -0500

    created more tests for the /:id route in users

--- api/tests/users.test.ts
+++ api/tests/users.test.ts
@@ -1,13 +1,46 @@
 import { describe, it, expect } from "bun:test"
+import { UserModel } from "@/modules/user/model"
+import { type UnwrapBodySchema } from "elysia"
 import { app } from '@/index'
 
-describe("Users", () => {
-    it("should return all users", async () => {
+describe("GET /users", () => {
+    it("should return 200", async () => {
         const response = await app
             .handle(new Request("http://localhost/api/users/"))
         
         expect(response.status).toBe(200)
+    })
+    it("should return array", async () => {
+        const response = await app
+            .handle(new Request("http://localhost/api/users/"))
+        
         const data = await response.json()
         expect(data).toBeArray()
     })
+})
+
+describe("GET /users/:id", () => {
+    it("should return 200", async () => {
+        const response = await app
+            .handle(new Request("http://localhost/api/users/7"))
+
+        expect(response.status).toBe(200)
+    })
+    it("should return 404", async () => {
+        const response = await app
+            .handle(new Request("http://localhost/api/users/999999"))
+
+        expect(response.status).toBe(404)
+    })
+    it("should return correct user", async () => {
+        const response = await app
+            .handle(new Request("http://localhost/api/users/7"))
+        
+        type UserResponse = typeof UserModel.response.static
+
+        const data = await response.json() as UserResponse
+        expect(data).toBeObject()
+        expect(data.username).toBe("jsmith")
+    })
+    
 })
